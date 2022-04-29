545f9123064148cd16980854c8924d82449c0002
Aaron J Spetner <aaron.spetner@benmanage.com>
2022-04-29 05:30:00 -0500

    Added 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import { Home as HomePage } from "./pages/Home";
 import { Missions as MissionsPage } from "./pages/Missions";
 import { Weather as WeatherPage } from "./pages/Weather";
 import { Preferences as PreferencesPage } from "./pages/Preferences";
+import { _404 } from "./pages/_404";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 
 function App() {
-  
   return (
     <HelmetProvider>
       <Helmet>
@@ -17,6 +17,7 @@ function App() {
         <Route path="/missions" element={<MissionsPage />} />
         <Route path="/weather" element={<WeatherPage />} />
         <Route path="/Preferences" element={<PreferencesPage />} />
+        <Route path="*" element={<_404 />} />
       </Routes>
     </HelmetProvider>
   );
---
+++ client/src/pages/_404.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { AppLayout } from "../layouts/AppLayout";
+export const _404 = (): JSX.Element => {
+  return (
+    <AppLayout title="404 - Not found">
+      <Typography variant="h2">404 - Page not found</Typography>
+      <Typography variant="h4">The page you are looking for was not found. Did you forget to bring a map?</Typography>
+    </AppLayout>
+  );
+};
